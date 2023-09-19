f2b5524927ae42ac7318659a1b1195fbaa52ac58
Anees ur Rehman <anees.rehman@zweidevs.com>
2023-09-19 16:12:42 +0500

    button common componet

--- src/lib/ts/interface.ts
+++ src/lib/ts/interface.ts
@@ -1,3 +1,4 @@
+import React, { FC, ReactNode, MouseEvent } from "react";
 export interface Props {
   selected_id?: string;
   handleLogout?: any;
@@ -5,4 +6,19 @@ export interface Props {
   hideModal?: any;
   children?: any;
   window?: () => Window;
+}
+
+export interface FusionEnergyButtonProps {
+  type?: "primary" | "default" | "dashed" | "link" | "text";
+  icon?: ReactNode;
+  loading?: boolean;
+  onClick?: (event: MouseEvent<HTMLElement>) => void;
+  disabled?: boolean;
+  className?: string;
+  htmlType?: "button" | "submit" | "reset";
+  color?: string;
+  height?:string;
+  width?:string;
+  backgroundColor?: string;
+  label: ReactNode;
 }
