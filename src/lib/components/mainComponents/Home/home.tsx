f2ef21d5d92892ccd28b36ddae260456ee2e51e9
Anees ur Rehman <anees.rehman@zweidevs.com>
2023-09-20 12:50:09 +0500

    folder name changed

--- src/lib/components/mainComponents/Home/home.tsx
+++ src/lib/components/mainComponents/Home/home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 interface PROPS {}
-import { FusionEnergyBackgroundImage } from "@/lib/components/commonComponets";
+import { FusionEnergyBackgroundImage } from "@/lib/components/commonComponents";
 const homeArray = [
   {
     title: "Solar Panel",
