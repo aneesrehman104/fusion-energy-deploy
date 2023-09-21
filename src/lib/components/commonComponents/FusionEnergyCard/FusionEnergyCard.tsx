48e665dafb11353fb14f58e9e089116888c4e6ff
Anees ur Rehman <anees.rehman@zweidevs.com>
2023-09-21 13:22:20 +0500

    Pr ccomments resolved

--- src/lib/components/commonComponents/FusionEnergyCard/FusionEnergyCard.tsx
+++ src/lib/components/commonComponents/FusionEnergyCard/FusionEnergyCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Image from 'next/image';
 import { Card, Rate } from 'antd';
 import { FusionEnergyCardProps } from '@/lib/ts/interface';
+import Image from 'next/image';
 import styles from './FusionEnergyCard.module.css';
 
 type Props = { item: FusionEnergyCardProps };
