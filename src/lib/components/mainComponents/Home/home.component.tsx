026f3a0fa0d4d434056d1c1524be697b474ff293
Anees ur Rehman <anees.rehman@zweidevs.com>
2023-09-26 13:45:09 +0500

    build size reduced

--- src/lib/components/mainComponents/Home/home.component.tsx
+++ src/lib/components/mainComponents/Home/home.component.tsx
@@ -1,8 +1,7 @@
+'use client';
 import React from 'react';
 import dynamic from 'next/dynamic';
 import { testimonials, staticHeading } from '@/utils/mock';
-
-const FinedOut = dynamic(() => import('./FinedOut'));
 const LowCost = dynamic(() => import('./LowCostComponets'));
 const WhoWeAre = dynamic(() => import('./WhoWeAre'));
 const FusionEnergyCarousel = dynamic(() =>
@@ -10,13 +9,33 @@ const FusionEnergyCarousel = dynamic(() =>
         (m) => m.FusionEnergyCarousel,
     ),
 );
+const Banner = dynamic(() =>
+    import('@/lib/components/layoutComponents').then((m) => m.Banner),
+);
 
 interface PROPS {}
 
 const Home: React.FC<PROPS> = () => {
+    const contactRef = React.useRef<HTMLDivElement>(null);
+    const onClickHandle = () => {
+        if (contactRef.current) {
+            contactRef.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+            });
+        }
+    };
     return (
         <>
-            <FinedOut />
+            <Banner
+                backgroundImage="/FusionEnergyEfficiencyBG.svg"
+                iconsArray={[]}
+                title={'Efficiency Is The Future'}
+                showButton
+                description={' Contact us today to take advantage'}
+                labelBtn={'Find Out How'}
+                onClick={onClickHandle}
+            />
             <LowCost />
             <WhoWeAre />
             <FusionEnergyCarousel
